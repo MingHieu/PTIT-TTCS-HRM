9b24245934dd1289775fb072d46c2605115819fa
MingHieu <hieubeo0612@gmail.com>
2023-02-21 12:49:19 +0700

    create more model

--- src/api.module.ts
+++ src/api.module.ts
@@ -3,6 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth/auth.module';
 import { PrismaModule } from 'src/database/prisma/prisma.module';
 import { UserModule } from 'src/model/user/user.module';
+import { NewsModule } from './model/news/news.module';
+import { AttendanceModule } from './model/attendance/attendance.module';
+import { SalaryModule } from './model/salary/salary.module';
+import { RequestModule } from './model/request/request.module';
+import { EventModule } from './model/event/event.module';
+import { NotificationModule } from './model/notification/notification.module';
 
 @Module({
   imports: [
@@ -12,6 +18,12 @@ import { UserModule } from 'src/model/user/user.module';
     PrismaModule,
     AuthModule,
     UserModule,
+    NewsModule,
+    AttendanceModule,
+    SalaryModule,
+    RequestModule,
+    EventModule,
+    NotificationModule,
   ],
   controllers: [],
   providers: [],
