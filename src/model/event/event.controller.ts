2d04daabffe2620da5a0e6a232ea7ede5f4af20d
MingHieu <hieubeo0612@gmail.com>
2023-03-26 18:50:30 +0700

    update event model

--- src/model/event/event.controller.ts
+++ src/model/event/event.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator';
 import { PaginationDto } from 'src/common/dto';
 import { ParseIntPipe } from 'src/common/pipe';
@@ -8,15 +8,7 @@ import { EventService } from './event.service';
 export class EventController {
   constructor(private eventService: EventService) {}
 
-  @Post('subscribe/:id')
-  subscribe(
-    @Param('id', ParseIntPipe) id: number,
-    @GetUser('username') username: string,
-  ) {
-    return this.eventService.subscribe(id, username);
-  }
-
-  @Get('all')
+  @Get()
   getMany(@Query() query: PaginationDto) {
     return this.eventService.getMany(
       query.page,
@@ -25,13 +17,16 @@ export class EventController {
     );
   }
 
-  @Get('all/:username')
-  getAllByUsername(@Param('username') username) {
-    return this.eventService.getAllByUsername(username);
+  @Post('subscribe')
+  subscribe(
+    @Body('id', ParseIntPipe) id: number,
+    @GetUser('username') username: string,
+  ) {
+    return this.eventService.subscribe(id, username);
   }
 
-  @Get(':id')
-  getOne(@Param('id', ParseIntPipe) id: number) {
-    return this.eventService.getOne(id);
+  @Get('subscribe')
+  getSubscribeList(@GetUser('username') username) {
+    return this.eventService.getAllByUsername(username);
   }
 }
